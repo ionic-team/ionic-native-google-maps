4f5e496028dd30b14dda971b8c3f82895e18967d
wf9a5m75 <wf9a5m75@gmail.com>
2018-08-31 22:22:20 -0700

    Fix: typo

--- src/@ionic-native/plugins/google-maps/index.ts
+++ src/@ionic-native/plugins/google-maps/index.ts
@@ -1105,7 +1105,7 @@ export const GoogleMapsMapTypeId = {
  * Demo:
  *  - [Browser platform](https://mapsplugin.github.io/ionic-googlemaps-quickdemo-v4/)
  *
- * Documentaion:
+ * Documentation:
  *  - [API Reference](https://github.com/ionic-team/ionic-native-google-maps#documentation)
  *  - [Overview slide](https://docs.google.com/presentation/d/e/2PACX-1vScoho1ensbR4qCI9AIuQN55BZVvK73pAjI7sumDvW3CrxxHnrmpXWUjx2-8CpFibqU1EjLKCRhuthJ/pub?start=false&loop=false&delayms=3000)
  *

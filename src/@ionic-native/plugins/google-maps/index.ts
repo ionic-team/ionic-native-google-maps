681864e84e6d2bd4d5d9018c2a8fc6ea5de47eb3
wf9a5m75 <wf9a5m75@gmail.com>
2018-12-29 19:42:28 -0800

    Fix: INFO_CLICK event does not work on marker cluster

--- src/@ionic-native/plugins/google-maps/index.ts
+++ src/@ionic-native/plugins/google-maps/index.ts
@@ -1349,25 +1349,26 @@ const displayErrorMessage = (element: HTMLElement, message: string) => {
   element.appendChild(errorDiv);
 };
 
-const normalizeArgumentsOfEventListener = (_objectInstance: any, ...args: any[]): any[] => {
+const normalizeArgumentsOfEventListener = (_objectInstance: any, args: any[]): any[] => {
   if (args[args.length - 1] instanceof GoogleMaps.getPlugin().BaseClass) {
     if (args[args.length - 1].type === 'Map' || args[args.length - 1].type === 'StreetViewPanorama') {
       args[args.length - 1] = this;
-    } else if (this instanceof MarkerCluster) {
-      let overlay: Marker = this.get(args[args.length - 1].getId());
+    } else if (_objectInstance.__pgmId.indexOf('markercluster_') !== -1) {
+      const _overlays: any = _objectInstance.get('_overlays') || {};
+      let overlay: Marker = _overlays[args[args.length - 1].getId()];
       if (!overlay) {
         const markerJS: any = args[args.length - 1];
         const markerId: string = markerJS.getId();
-        const markerCluster: MarkerCluster = this as MarkerCluster;
+        const markerCluster: MarkerCluster = _objectInstance as MarkerCluster;
         overlay = new Marker(markerCluster.getMap(), markerJS);
-        this.get('_overlays')[markerId] = overlay;
+        _objectInstance.get('_overlays')[markerId] = overlay;
         markerJS.one(markerJS.getId() + '_remove', () => {
-          this.get('_overlays')[markerId] = null;
+          _objectInstance.get('_overlays')[markerId] = null;
         });
       }
       args[args.length - 1] = overlay;
     } else {
-      args[args.length - 1] = this._objectInstance.getMap().get('_overlays')[args[args.length - 1].getId()];
+      args[args.length - 1] = _objectInstance.getMap().get('_overlays')[args[args.length - 1].getId()];
     }
   }
   return args;
@@ -1406,7 +1407,7 @@ export class BaseClass {
   addEventListener(eventName: string): Observable<any> {
     return new Observable((observer) => {
       this._objectInstance.addEventListener(eventName, (...args: any[]) => {
-        const newArgs = normalizeArgumentsOfEventListener(this._objectInstance, args);
+        const newArgs = normalizeArgumentsOfEventListener.call(this, this._objectInstance, args);
         observer.next(newArgs);
       });
     });
@@ -1422,7 +1423,7 @@ export class BaseClass {
   // addThrottledEventListener(eventName: string, throttleInterval: number): Observable<any> {
   //   return new Observable((observer) => {
   //     this._objectInstance.addThrottledEventListener(eventName, (...args: any[]) => {
-  //       const newArgs = normalizeArgumentsOfEventListener(this._objectInstance, args);
+  //       const newArgs = normalizeArgumentsOfEventListener.call(this, this._objectInstance, args);
   //       observer.next(newArgs);
   //     });
   //   });
@@ -1438,7 +1439,7 @@ export class BaseClass {
   addEventListenerOnce(eventName: string): Promise<any> {
     return new Promise<any>((resolve) => {
       this._objectInstance.one(eventName, (...args: any[]) => {
-        const newArgs = normalizeArgumentsOfEventListener(this._objectInstance, args);
+        const newArgs = normalizeArgumentsOfEventListener.call(this, this._objectInstance, args);
         resolve(newArgs);
       });
     });
@@ -1495,8 +1496,8 @@ export class BaseClass {
   on(eventName: string): Observable<any> {
     return new Observable((observer) => {
       this._objectInstance.on(eventName, (...args: any[]) => {
-        const newArgs = normalizeArgumentsOfEventListener(this._objectInstance, args);
-        observer.next(args);
+        const newArgs = normalizeArgumentsOfEventListener.call(this, this._objectInstance, args);
+        observer.next(newArgs);
       });
     });
   }
@@ -1510,7 +1511,7 @@ export class BaseClass {
   // onThrottled(eventName: string): Observable<any> {
   //   return new Observable((observer) => {
   //     this._objectInstance.onThrottled(eventName, (...args: any[]) => {
-  //       const newArgs = normalizeArgumentsOfEventListener(this._objectInstance, args);
+  //       const newArgs = normalizeArgumentsOfEventListener.call(this, this._objectInstance, args);
   //       observer.next(newArgs);
   //     });
   //   });
@@ -1525,7 +1526,7 @@ export class BaseClass {
   one(eventName: string): Promise<any> {
     return new Promise<any>((resolve) => {
       this._objectInstance.one(eventName, (...args: any[]) => {
-        const newArgs = normalizeArgumentsOfEventListener(this._objectInstance, args);
+        const newArgs = normalizeArgumentsOfEventListener.call(this, this._objectInstance, args);
         resolve(newArgs);
       });
     });

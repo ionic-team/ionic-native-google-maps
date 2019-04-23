54ab65841b6a64f1a8c30b8f727c2d17e1f7e233
wf9a5m75 <wf9a5m75@gmail.com>
2019-04-23 09:33:57 -0700

    revert: edit of index.ts

--- src/@ionic-native/plugins/google-maps/index.ts
+++ src/@ionic-native/plugins/google-maps/index.ts
@@ -6,8 +6,7 @@ import {
   IonicNativePlugin,
   Plugin,
   checkAvailability,
-  getPromise,
-  Cordova
+  getPromise
 } from '@ionic-native/core';
 import { Observable } from 'rxjs';
 
@@ -2067,16 +2066,64 @@ export class Environment {
   plugin: 'cordova-plugin-googlemaps',
   repo: ''
 })
-export class Geocoder extends IonicNativePlugin {
+export class Geocoder {
+
+  /**
+   * @deprecation This method is static. Please use Geocoder.geocode()
+   * @hidden
+   */
+  geocode(request: GeocoderRequest): Promise<GeocoderResult[] | BaseArrayClass<GeocoderResult[]>> {
+    console.error('GoogleMaps', '[deprecated] This method is static. Please use Geocoder.geocode()');
+    return Geocoder.geocode(request);
+  }
+
   /**
    * Converts position to address and vice versa
    * @param {GeocoderRequest} request Request object with either an address or a position
    * @return {Promise<GeocoderResult[] | BaseArrayClass<GeocoderResult>>}
    */
-  @Cordova({ sync: true })
-  geocode(request: GeocoderRequest): Promise<GeocoderResult[] | BaseArrayClass<GeocoderResult[]>> {
-    return null;
+  static geocode(request: GeocoderRequest): Promise<GeocoderResult[] | BaseArrayClass<GeocoderResult[]>> {
+
+    if (checkAvailability(GoogleMaps.getPluginRef(), null, GoogleMaps.getPluginName()) === false) {
+      throw new Error('cordova-plugin-googlemaps is not ready. Please use platform.ready() before accessing this method.');
+    }
+    if (request.address instanceof Array || Array.isArray(request.address) ||
+      request.position instanceof Array || Array.isArray(request.position)) {
+      // -------------------------
+      // Geocoder.geocode({
+      //   address: [
+      //    "Kyoto, Japan",
+      //    "Tokyo, Japan"
+      //   ]
+      // })
+      // -------------------------
+      return getPromise<BaseArrayClass<GeocoderResult[]>>((resolve, reject) => {
+        GoogleMaps.getPlugin().Geocoder.geocode(request, (mvcArray: any) => {
+          if (mvcArray) {
+            resolve(new BaseArrayClass(mvcArray));
+          } else {
+            reject();
+          }
+        });
+      });
+    } else {
+      // -------------------------
+      // Geocoder.geocode({
+      //   address: "Kyoto, Japan"
+      // })
+      // -------------------------
+      return getPromise<GeocoderResult[]>((resolve, reject) => {
+        GoogleMaps.getPlugin().Geocoder.geocode(request, (results: GeocoderResult[]) => {
+          if (results) {
+            resolve(results);
+          } else {
+            reject();
+          }
+        });
+      });
+    }
   }
+
 }
 
 /**
